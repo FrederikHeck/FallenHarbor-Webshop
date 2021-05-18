14a201ef975402e21fdb0cbcd4706bc1caec02c7
Frederik Heck <fredi.h@posteo.de>
2021-05-18 16:46:29 +0200

    remove files

--- assets/content/word.php
+++ assets/content/word.php
@@ -1,7 +1,7 @@
 <h1><?=$dict["word"][$lng]?></h1>
 <h2 class="anmerkungen">Anmerkungen zum Shop</h2>
 <ul class="anmerkungen">
-    <li class="anmerkungen">Keine semantischen URLs, kein Backend</li>
+    <li class="anmerkungen">Keine semantischen URLs, kein Admin-Bereich mit erhöter FUnktionalität</li>
     <li class="anmerkungen">Ajax ist sowohl im Warenkorb als auch im Form zur User-Registrierung implementiert.
     <li class="anmerkungen">Die Tracklist-Funktion lädt immer die gleiche Tracklist.</li>
     <li class="anmerkungen">Die Change-Funktion auf der Buy-Site (Final Review) ist nur für Gäste angepasst, nicht für registrierte Nutzer,
